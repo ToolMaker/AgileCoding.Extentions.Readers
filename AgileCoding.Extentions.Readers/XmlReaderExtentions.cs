5392d659aa9eb5ba9749b15e4f63b8f40221602b
Ernie <gunning.ernie@gmail.com>
2021-06-01 20:17:37 +0200

    Package start

---
+++ AgileCoding.Extentions.Readers/XmlReaderExtentions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace AgileCoding.Extentions.Readers
+{
+    public static class XmlReaderExtentions
+    {
+        public static bool ReadToNextElement(this XmlReader reader)
+        {
+            reader.Read();
+            var nodeTypeName = reader.LocalName;
+            do
+            {
+                int depth = reader.Depth;
+                nodeTypeName = reader.LocalName;
+                var elementName = reader.GetAttribute("name");
+
+                if (nodeTypeName == "element" && reader.NodeType == XmlNodeType.Element)
+                {
+                    break;
+                }
+
+                reader.Read();
+            }
+            while (!reader.EOF && nodeTypeName != "element");
+
+            return reader.EOF;
+        }
+    }
+}
---
+++ ReadMePage.md
@@ -0,0 +1,5 @@
+﻿#AgileCoding.Extentions.Readers ReadMePage
+
+###Extensions related to Readers
+
+1.	XmlReaderExtentions - Static class containing xml reader extentions
